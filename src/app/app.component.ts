449a84ce84668b04f13efdb137c8585c27f30025
tvdvl <vtvlugt@outlook.com>
2018-08-14 16:11:55 +0200

    Internal App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'CalcUtilAng';
+  title = 'G-REKENING CALCULATOR';
+  public grekeningForm: 0;
+  public subtotal: number;
+  public userinput: number;
+  public selectedTaxType: number;
+  public gtotal: number;
+  public copyTotal: string;
+  public isCopied: boolean;
+
+  public taxCodes = [
+    {name: 'NL-HOOG', taxvalue: 0.21},
+    {name: 'NL-LAAG', taxvalue: 0.06},
+    {name: 'NL-NUL', taxvalue: 0.00}
+  ];
+
+  selected($event): void {
+    console.log($event.target.value);
+    this.selectedTaxType = $event.target.value;
+  }
+
+  clear(): void {
+    this.subtotal = null;
+    this.userinput = null;
+    this.gtotal = null;
+  }
+
+  calculate(): void {
+    // TODO: FORM VALIDATIE
+    this.gtotal = (this.subtotal) * (this.userinput / 100) + (this.selectedTaxType * this.subtotal);
+    console.log(this.selectedTaxType);
+    console.log(this.gtotal);
+  }
+  copyToClipboard(): void {
+    this.copyTotal = this.gtotal.toString(10);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule, Validators} from '@angular/forms';
+
+import { ClipboardModule} from 'ngx-clipboard';
+import { ClarityModule, ClrFormsNextModule } from '@clr/angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 import { AppComponent } from './app.component';
 
@@ -8,9 +14,19 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ClarityModule,
+    BrowserAnimationsModule,
+    ClrFormsNextModule,
+    FormsModule,
+    ClipboardModule
+  ],
+  exports: [
+    ClarityModule,
+    ClrFormsNextModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
